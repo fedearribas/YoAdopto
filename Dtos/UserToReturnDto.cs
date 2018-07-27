7564295a5edea6017af60ce5c246c9279c4e9ccc
Federico Arribas <fede.arribas@gmail.com>
2018-07-27 18:11:46 -0300

    Added authentication

--- Dtos/UserToReturnDto.cs
+++ Dtos/UserToReturnDto.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Collections.Generic;
+using YoAdopto.API.Models;
 
-namespace YoAdopto.API.Models
+namespace YoAdopto.API.Dtos
 {
-    public class User
+    public class UserToReturnDto
     {
         public int Id { get; set; }
         public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Email { get; set; }        
         public ICollection<Publication> Publications { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime LastActive { get; set; }
--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 using YoAdopto.API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using AutoMapper;
 
 namespace YoAdopto.API
 {
@@ -26,7 +31,24 @@ namespace YoAdopto.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>(x => x.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+            
+                        ValidIssuer = "http://localhost:5000",
+                        ValidAudience = "http://localhost:5000",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"))
+                    };
+                });
+ 
+            services.AddAutoMapper();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -41,7 +63,8 @@ namespace YoAdopto.API
             {
                 app.UseHsts();
             }
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
+            app.UseAuthentication();      
             app.UseHttpsRedirection();
             app.UseMvc();
         }
