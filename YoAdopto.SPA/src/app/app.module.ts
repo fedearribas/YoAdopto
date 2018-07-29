e11724b6c2efe2ffea4ebc6a43a6bea7664b4eee
Federico Arribas <fede.arribas@gmail.com>
2018-07-29 18:03:58 -0300

    Added navbar and home page

--- YoAdopto.SPA/src/app/app.module.ts
+++ YoAdopto.SPA/src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AppRouters } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CollapseModule } from 'ngx-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      WelcomeComponent,
+      NavbarComponent
+   ],
+   imports: [
+      BrowserModule,
+      CollapseModule.forRoot(),
+      AppRouters
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
